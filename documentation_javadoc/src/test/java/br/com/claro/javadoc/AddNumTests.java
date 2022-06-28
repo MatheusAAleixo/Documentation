2921301c605a4508052072dd752fe2684f7f2764
MatheusAAleixo <83460598+MatheusAAleixo@users.noreply.github.com>
2022-06-28 03:22:01 -0300

    Add files via upload

---
+++ documentation_javadoc/src/test/java/br/com/claro/javadoc/AddNumTests.java
@@ -0,0 +1,13 @@
+package br.com.claro.javadoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddNumTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
