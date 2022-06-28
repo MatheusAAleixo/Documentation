2ecf16cc4282e0cd84d525829119930ddcccbc80
MatheusAAleixo <83460598+MatheusAAleixo@users.noreply.github.com>
2022-06-28 03:19:25 -0300

    Add files via upload

---
+++ documentation_swagger/src/main/java/br/com/claro/swagger/DocumentationSwaggerApplication.java
@@ -0,0 +1,13 @@
+package br.com.claro.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentationSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentationSwaggerApplication.class, args);
+	}
+
+}
